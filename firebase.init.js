8df46456d53f63d05766b3fb7ca114eb5b88178c
Jawhar <jawharkoonbhani@gmail.com>
2025-10-01 17:38:06 +0500

    Update firebase.init.js

--- firebase.init.js
+++ firebase.init.js
@@ -1,11 +1,5 @@
 const firebaseConfig = {
-  apiKey: "AIzaSyCHyyK1WXT5BL3Dtt1xv_ujK5tNovF1mZM",
-  authDomain: "to-do-list-701d9.firebaseapp.com",
-  databaseURL: "https://to-do-list-701d9-default-rtdb.firebaseio.com",
-  projectId: "to-do-list-701d9",
-  storageBucket: "to-do-list-701d9.firebasestorage.app",
-  messagingSenderId: "608578577023",
-  appId: "1:608578577023:web:d9679eb45f6c4da5bfd54a",
+  
 };
 
 // firebase.initializeApp(firebaseConfig);
